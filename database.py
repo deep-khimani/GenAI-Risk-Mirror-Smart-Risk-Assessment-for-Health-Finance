2ca85c63df231c4773f37a53a8fa74bea4125690
Deep Khimani <deepkhimani@gmail.com>
2025-08-26 09:46:04 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,9 +7,7 @@ db = client[Config.DB_NAME]
 risk_analysis_collection = db["risk_analyses"]
 
 def save_analysis(analysis_data):
-    """Save risk analysis to MongoDB"""
     return risk_analysis_collection.insert_one(analysis_data)
 
 def get_analysis_history():
-    """Get all previous risk analyses"""
     return list(risk_analysis_collection.find({}, {"_id": 0}))
